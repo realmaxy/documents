79e03b77ed107aee34bb7ff80f209eb37bb98423
realmaxy <max_sem777@mail.ru>
2023-04-21 21:00:27 +0300

    finished to design firstScreen

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,12 +1,12 @@
 import s from './Landing.module.scss'
 import Header from '../Header/Header'
+import ScrollBox from '../ScrollBox/ScrollBox'
 
 export default function Landing() {
   return (
     <div className={s.landing}>
         <Header/>
-      <div className='container'>
-      </div>
+        <ScrollBox/>
     </div>
   )
 }
---
+++ src/components/ScrollBox/ScrollBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FirstScreen from '../FirstScreen/FirstScreen'
+
+export default function ScrollBox() {
+  return (
+    <div>
+      <FirstScreen/>
+    </div>
+  )
+}
